da1df420f797338a042912c28dbedf2d16d8d276
Cole McIntosh <colemcintosh6@gmail.com>
2023-07-07 13:42:57 -0500

    Update main.py, README.md, reqs.txt, and gitignore

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# code-qa
+# Script Sleuth 🕵️‍♂️
+
+This project is a Python script that allows you to interactively explore your codebase. It's designed to find all programming files in a specified directory and its subdirectories, and then uses OpenAI's GPT-4 model to answer any questions you have about a selected file.
+
+## Getting Started
+
+### Prerequisites
+
+Before you can run this script, you'll need to have the following installed on your machine:
+
+- Python 3.8 or higher
+- pip (Python's package installer)
+
+### Installation
+
+To get started with this project, follow these steps:
+
+1. Clone the repository to your local machine:
+```bash
+git clone https://github.com/colesmcintosh/code-qa
+```
+
+2. Navigate to the project directory:
+```bash
+cd code-qa
+```
+
+3. Install the necessary Python packages:
+```bash
+pip install -r requirements.txt
+```
+
+4. Create a file in the root project directory named `.env` and add your OpenAI API key as an environment variable:
+```bash
+OPENAI_API_KEY=your_openai_api_key
+```
+Replace `your_openai_api_key` with your actual OpenAI API key.
+
+### Execution
+
+Once you've installed all prerequisites and set up your environment variables, you can run the script with the following command:
+```bash
+python main.py
+```
+
+## Usage
+
+When you run the script, it will first ask you to enter a directory. This should be the root directory of the codebase you want to explore. The script will then find all programming files in that directory and its subdirectories, presenting you with a list of these files.
+
+Next, the script will ask you to enter the number of the file you're interested in. Once you've selected a file, you will be prompted to input a question about the selected file. The script will then use the GPT-4 model to generate an answer to your question and print it out.
+
+## Code Overview
+
+The script uses Python's `glob` and `os` modules to find all programming files in the specified directory and its subdirectories. The `dotenv` module is used to load environment variables, including your OpenAI API key, which is used to authenticate with the OpenAI API.
+
+The `langchain` library is used for language model generation, specifically the `ChatOpenAI` class for interaction with the OpenAI API, and the `ChatPromptTemplate`, `SystemMessagePromptTemplate`, and `HumanMessagePromptTemplate` classes for prompt creation and formatting.
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are greatly appreciated.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+# Footnote
+
+This README was generated using the very script it describes - metaaaaa! 🤩
--- main.py
+++ main.py
@@ -1,36 +1,94 @@
-import warnings
-warnings.filterwarnings('ignore')
-from pprint import pprint
-from langchain.document_loaders.generic import GenericLoader
-from langchain.document_loaders.parsers import LanguageParser
-from langchain.text_splitter import (
-    RecursiveCharacterTextSplitter,
-    Language,
-)
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+"""
+This script interacts with the user to provide explanations for
+selected code files. It uses the OpenAI model "gpt-4" for generating the explanations.
+"""
 
-wd = "/Users/cole.mcintosh/Desktop/PyAutomations/Action-Center/elc"
-
-loader = GenericLoader.from_filesystem(
-    wd,
-    glob="*",
-    suffixes=[".py"],
-    parser=LanguageParser()
-)
-docs = loader.load()
-
-py_splitter = RecursiveCharacterTextSplitter.from_language(
-    language=Language.PYTHON, chunk_size=60, chunk_overlap=0
+import os
+import glob
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
 )
 
-result = py_splitter.split_documents(docs)
+# Load the environment variables
+load_dotenv()
 
-embeddings = HuggingFaceEmbeddings()
+def fetch_code_file():
+    """
+    Function to fetch programming files based on the provided directory
+    and chosen file number
+    """
 
-db = FAISS.from_documents(docs, embeddings)
+    # Ask the user for the directory
+    root_dir = input("Enter the directory: ")
 
-query = "What is the endpoint used?"
-docs = db.similarity_search(query)
+    # Find all Python files in the directory and subdirectories
+    lang_exts = ['*.py', '*.html', '*.css', '*.rb', '*.c', '*.js', '*.md', '*.txt']
+    programming_files = []
+    for extension in lang_exts:
+        programming_files.extend(glob.glob(os.path.join(root_dir, '**', extension), recursive=True))
 
-pprint(docs)
+    # Print all Python files
+    for i, file in enumerate(programming_files, start=1):
+        print(f"[{i}] {file.replace(root_dir, '').replace('/', '')}")
+
+    # Ask the user for the file they're interested in
+    file_num = int(input("Enter the number of the file you have questions about: "))
+
+    # Get the full path of the selected file
+    selected_file = programming_files[file_num - 1]
+
+    # Read the contents of the selected file
+    with open(selected_file, 'r') as file:
+        file_contents = file.read()
+
+    return root_dir, selected_file, file_contents
+
+def format_prompt(file_contents, selected_file, root_dir):
+    """
+    Function to format the user prompt with the ChatOpenAI model
+    """
+
+    template = (
+        "You are a helpful assistant that answers questions or fulfills requests based on code files. Format the answer as a markdown file but do not include any text alterations such as bold, italics, or headers."
+    )
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = """Code Base: {code_base}
+    Based on the code base above please provided a detailed answer with references to the code base to the following: {question}"""
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    code_base = file_contents
+    question = input(f"What questions do you have about {selected_file.replace(root_dir, '').replace('/', '')}? ")
+
+    prompt_formatted = chat_prompt.format_prompt(
+            code_base=code_base, question=question
+    ).to_messages()
+
+    return prompt_formatted
+
+def main():
+    """
+    Main function to orchestrate the operations
+    """
+
+    root_dir, selected_file, file_contents = fetch_code_file()
+
+    prompt_formatted = format_prompt(file_contents, selected_file, root_dir)
+
+    chat = ChatOpenAI(model_name="gpt-4", temperature=0)
+
+    # get a chat completion from the formatted messages
+    result = chat(prompt_formatted)
+
+    print("------------ RESULT ------------")
+    print(result.content)
+
+if __name__ == '__main__':
+    main()
