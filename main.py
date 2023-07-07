50a8e2bfae35e29d105e7e926718ef5d50a64c5a
Cole McIntosh <colemcintosh6@gmail.com>
2023-07-07 14:04:40 -0500

    Update UX for changing files

--- main.py
+++ main.py
@@ -21,31 +21,38 @@ def fetch_code_file():
     Function to fetch programming files based on the provided directory
     and chosen file number
     """
+    while True:
+        # Ask the user for the directory
+        root_dir = input("Enter the directory (or 'exit' to quit): ")
 
-    # Ask the user for the directory
-    root_dir = input("Enter the directory: ")
+        if root_dir.lower() == 'exit':
+            return None, None, None
 
-    # Find all Python files in the directory and subdirectories
-    lang_exts = ['*.py', '*.html', '*.css', '*.rb', '*.c', '*.js', '*.md', '*.txt']
-    programming_files = []
-    for extension in lang_exts:
-        programming_files.extend(glob.glob(os.path.join(root_dir, '**', extension), recursive=True))
+        # Find all programming files in the directory and subdirectories
+        lang_exts = ['*.py', '*.html', '*.css', '*.rb', '*.c', '*.js', '*.md', '*.txt']
+        programming_files = []
+        for extension in lang_exts:
+            programming_files.extend(glob.glob(os.path.join(root_dir, '**', extension), recursive=True))
 
-    # Print all Python files
-    for i, file in enumerate(programming_files, start=1):
-        print(f"[{i}] {file.replace(root_dir, '').replace('/', '')}")
+        # Print all Python files
+        for i, file in enumerate(programming_files, start=1):
+            print(f"[{i}] {file.replace(root_dir, '').replace('/', '')}")
 
-    # Ask the user for the file they're interested in
-    file_num = int(input("Enter the number of the file you have questions about: "))
+        # Ask the user for the file they're interested in
+        file_num = input("Enter the number of the file you have questions about (or 'back' to choose a new directory): ")
 
-    # Get the full path of the selected file
-    selected_file = programming_files[file_num - 1]
+        if file_num.lower() == 'back':
+            continue
 
-    # Read the contents of the selected file
-    with open(selected_file, 'r') as file:
-        file_contents = file.read()
+        # Get the full path of the selected file
+        selected_file = programming_files[int(file_num) - 1]
 
-    return root_dir, selected_file, file_contents
+        # Read the contents of the selected file
+        with open(selected_file, 'r') as file:
+            file_contents = file.read()
+
+        return root_dir, selected_file, file_contents
+
 
 def format_prompt(file_contents, selected_file, root_dir):
     """
@@ -65,30 +72,41 @@ def format_prompt(file_contents, selected_file, root_dir):
     )
 
     code_base = file_contents
-    question = input(f"What questions do you have about {selected_file.replace(root_dir, '').replace('/', '')}? ")
+    question = input(f"What questions do you have about {selected_file.replace(root_dir, '').replace('/', '')} (or 'back' to choose a new file'): ")
 
     prompt_formatted = chat_prompt.format_prompt(
             code_base=code_base, question=question
     ).to_messages()
 
-    return prompt_formatted
+    return prompt_formatted, question
 
 def main():
     """
     Main function to orchestrate the operations
     """
+    while True:
+        root_dir, selected_file, file_contents = fetch_code_file()
 
-    root_dir, selected_file, file_contents = fetch_code_file()
+        if root_dir is None:
+            break
 
-    prompt_formatted = format_prompt(file_contents, selected_file, root_dir)
+        while True:
+            prompt_formatted, question = format_prompt(file_contents, selected_file, root_dir)
 
-    chat = ChatOpenAI(model_name="gpt-4", temperature=0)
+            if question.lower() in ['exit', 'quit']:
+                return
+            elif question.lower() == 'back':
+                break
 
-    # get a chat completion from the formatted messages
-    result = chat(prompt_formatted)
+            chat = ChatOpenAI(model_name="gpt-4", temperature=0)
 
-    print("------------ RESULT ------------")
-    print(result.content)
+            # get a chat completion from the formatted messages
+            result = chat(prompt_formatted)
+
+            print("------------ RESULT ------------")
+            print(result.content)
+
 
 if __name__ == '__main__':
     main()
+
